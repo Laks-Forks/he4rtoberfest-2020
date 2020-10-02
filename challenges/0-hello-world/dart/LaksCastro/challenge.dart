dac59738b760c05f22848b71d65e7161fda81c1e
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-02 00:04:50 -0300

    add challenge 0

---
+++ challenges/0-hello-world/dart/LaksCastro/challenge.dart
@@ -0,0 +1,337 @@
+import 'dart:io';
+
+void main() {
+  H();
+  e();
+  l();
+  l();
+  o();
+  __();
+  W();
+  o();
+  r();
+  l();
+  d();
+}
+
+void a() {
+  stdout.write(_('a'));
+}
+
+void b() {
+  stdout.write(_('b'));
+}
+
+void c() {
+  stdout.write(_('c'));
+}
+
+void d() {
+  stdout.write(_('d'));
+}
+
+void e() {
+  stdout.write(_('e'));
+}
+
+void f() {
+  stdout.write(_('f'));
+}
+
+void g() {
+  stdout.write(_('g'));
+}
+
+void h() {
+  stdout.write(_('h'));
+}
+
+void i() {
+  stdout.write(_('i'));
+}
+
+void j() {
+  stdout.write(_('j'));
+}
+
+void k() {
+  stdout.write(_('k'));
+}
+
+void l() {
+  stdout.write(_('l'));
+}
+
+void m() {
+  stdout.write(_('m'));
+}
+
+void n() {
+  stdout.write(_('n'));
+}
+
+void o() {
+  stdout.write(_('o'));
+}
+
+void p() {
+  stdout.write(_('p'));
+}
+
+void q() {
+  stdout.write(_('q'));
+}
+
+void r() {
+  stdout.write(_('r'));
+}
+
+void s() {
+  stdout.write(_('s'));
+}
+
+void t() {
+  stdout.write(_('t'));
+}
+
+void u() {
+  stdout.write(_('u'));
+}
+
+void v() {
+  stdout.write(_('v'));
+}
+
+void w() {
+  stdout.write(_('w'));
+}
+
+void x() {
+  stdout.write(_('x'));
+}
+
+void y() {
+  stdout.write(_('y'));
+}
+
+void z() {
+  stdout.write(_('z'));
+}
+
+void A() {
+  stdout.write(_('A'));
+}
+
+void B() {
+  stdout.write(_('B'));
+}
+
+void C() {
+  stdout.write(_('C'));
+}
+
+void D() {
+  stdout.write(_('D'));
+}
+
+void E() {
+  stdout.write(_('E'));
+}
+
+void F() {
+  stdout.write(_('F'));
+}
+
+void G() {
+  stdout.write(_('G'));
+}
+
+void H() {
+  stdout.write(_('H'));
+}
+
+void I() {
+  stdout.write(_('I'));
+}
+
+void J() {
+  stdout.write(_('J'));
+}
+
+void K() {
+  stdout.write(_('K'));
+}
+
+void L() {
+  stdout.write(_('l'));
+}
+
+void M() {
+  stdout.write(_('M'));
+}
+
+void N() {
+  stdout.write(_('N'));
+}
+
+void O() {
+  stdout.write(_('O'));
+}
+
+void P() {
+  stdout.write(_('P'));
+}
+
+void Q() {
+  stdout.write(_('Q'));
+}
+
+void R() {
+  stdout.write(_('R'));
+}
+
+void S() {
+  stdout.write(_('S'));
+}
+
+void T() {
+  stdout.write(_('T'));
+}
+
+void U() {
+  stdout.write(_('U'));
+}
+
+void V() {
+  stdout.write(_('V'));
+}
+
+void W() {
+  stdout.write(_('W'));
+}
+
+void X() {
+  stdout.write(_('X'));
+}
+
+void Y() {
+  stdout.write(_('y'));
+}
+
+void Z() {
+  stdout.write(_('Zz'));
+}
+
+void __() {
+  stdout.write(_(' '));
+}
+
+String _(l) {
+  if (l == 'A') {
+    return 'A';
+  } else if (l == 'B') {
+    return 'B';
+  } else if (l == 'C') {
+    return 'C';
+  } else if (l == 'D') {
+    return 'D';
+  } else if (l == 'E') {
+    return 'E';
+  } else if (l == 'F') {
+    return 'F';
+  } else if (l == 'G') {
+    return 'G';
+  } else if (l == 'H') {
+    return 'H';
+  } else if (l == 'I') {
+    return 'I';
+  } else if (l == 'J') {
+    return 'J';
+  } else if (l == 'K') {
+    return 'K';
+  } else if (l == 'L') {
+    return 'L';
+  } else if (l == 'M') {
+    return 'M';
+  } else if (l == 'N') {
+    return 'N';
+  } else if (l == 'O') {
+    return 'O';
+  } else if (l == 'P') {
+    return 'P';
+  } else if (l == 'Q') {
+    return 'Q';
+  } else if (l == 'R') {
+    return 'R';
+  } else if (l == 'S') {
+    return 'S';
+  } else if (l == 'T') {
+    return 'T';
+  } else if (l == 'U') {
+    return 'U';
+  } else if (l == 'V') {
+    return 'V';
+  } else if (l == 'W') {
+    return 'W';
+  } else if (l == 'X') {
+    return 'X';
+  } else if (l == 'Y') {
+    return 'Y';
+  } else if (l == 'Z') {
+    return 'Z';
+  } else if (l == 'a') {
+    return 'a';
+  } else if (l == 'b') {
+    return 'b';
+  } else if (l == 'c') {
+    return 'c';
+  } else if (l == 'd') {
+    return 'd';
+  } else if (l == 'e') {
+    return 'e';
+  } else if (l == 'f') {
+    return 'f';
+  } else if (l == 'g') {
+    return 'g';
+  } else if (l == 'h') {
+    return 'h';
+  } else if (l == 'i') {
+    return 'i';
+  } else if (l == 'j') {
+    return 'j';
+  } else if (l == 'k') {
+    return 'k';
+  } else if (l == 'l') {
+    return 'l';
+  } else if (l == 'm') {
+    return 'm';
+  } else if (l == 'n') {
+    return 'n';
+  } else if (l == 'o') {
+    return 'o';
+  } else if (l == 'p') {
+    return 'p';
+  } else if (l == 'q') {
+    return 'q';
+  } else if (l == 'r') {
+    return 'r';
+  } else if (l == 's') {
+    return 's';
+  } else if (l == 't') {
+    return 't';
+  } else if (l == 'u') {
+    return 'u';
+  } else if (l == 'v') {
+    return 'v';
+  } else if (l == 'w') {
+    return 'w';
+  } else if (l == 'x') {
+    return 'x';
+  } else if (l == 'y') {
+    return 'y';
+  } else if (l == 'z') {
+    return 'z';
+  }
+
+  return ' ';
+}
---
+++ challenges/0-hello-world/dart/LaksCastro/readme.md
@@ -0,0 +1,19 @@
+# Submissão de Exercicio
+
+**Exercicio:** 0 - Hello World
+
+**Nickname:** LaksCastro
+
+**Nível Técnico:** - Estudante -
+
+**Dificuldade de Resolução:** - Alta -
+
+**Comentários:** gg
+
+**Como rodar o desafio**:
+
+Use o comando abaixo:
+
+```bash
+dart challenge.dart
+```
